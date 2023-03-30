ca14260d8b0b962e71805f646f9bc71c27189f57
flo-314 <julianflo1916@gmail.com>
2023-03-30 16:30:20 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,28 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
-import CanvasDraw from "react-canvas-draw";
+import CanvasDraw, {CanvasDrawProps} from "react-canvas-draw";
 
 export default function Home() {
   const canvasRef = useRef<CanvasDraw | null>(null);
-
+  const [CanvasDrawProp, setCanvasDrawProps] = useState(( {brushColor: "#0000000"}))
   return (
     <div className="w-full h-[100vh] my-auto">
 
       <div className=" w-full flex justify-center items-center">
-        <CanvasDraw className="" ref={canvasRef} />
+        <CanvasDraw className="" {...CanvasDrawProp} ref={canvasRef} />
       </div>
 
       <div className="flex  gap-6 justify-center items-center">
 
 
-<button>lapiz</button>
-<button>negro</button>
-<button>blanco</button>
+
+<button onClick={() => {
+setCanvasDrawProps({ ...CanvasDrawProp, brushColor: "#FFFFFFFF" });
+}}>negro</button>
+<button >borrar</button>
 <button>guardar</button>
-<button>cargar</button>
+<button >cargar</button>
 <div onClick={() => {
       if (canvasRef) {
         canvasRef.current?.undo();
