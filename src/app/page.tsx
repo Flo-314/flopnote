8edfb5dadc49effa0b857fecb9979fca57c4b40e
flo-314 <julianflo1916@gmail.com>
2023-03-30 15:58:48 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+"use client";
+import { useRef } from "react";
 
+import CanvasDraw from "react-canvas-draw";
 
 export default function Home() {
+  const canvasRef = useRef<CanvasDraw | null>(null);
+
   return (
- <div className="text-8xl text-8xl">holaa</div>
-  )
+    <div className="w-full h-[100vh] my-auto">
+
+      <div className=" w-full flex justify-center items-center">
+        <CanvasDraw className="" ref={canvasRef} />
+      </div>
+
+      <div className="flex  gap-6 justify-center items-center">
+
+
+<button>lapiz</button>
+<button>negro</button>
+<button>blanco</button>
+<button>guardar</button>
+<button>cargar</button>
+<div onClick={() => {
+      if (canvasRef) {
+        canvasRef.current?.undo();
+      }
+}}>undo</div>
+</div>
+    </div>
+  );
 }
