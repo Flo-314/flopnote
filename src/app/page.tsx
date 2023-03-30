e59e3562cc8a6bb406a5ee001b1b024654fa4a20
flo-314 <julianflo1916@gmail.com>
2023-03-30 19:40:35 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,57 @@
 "use client";
-import { useRef, useState } from "react";
-
-import CanvasDraw, {CanvasDrawProps} from "react-canvas-draw";
+import SignatureCanvas from 'react-signature-canvas'
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
-  const canvasRef = useRef<CanvasDraw | null>(null);
-  const [CanvasDrawProp, setCanvasDrawProps] = useState(( {brushColor: "#0000000"}))
+  const [signature, setSignature] = useState<SignatureCanvas | null>(null);
+  const [pageIndex, setPageIndex] = useState<number>(0)
+  const [pageData, setPageData] = useState<Array<Array<SignaturePad.Point>>[]>([])
+  const handleClear = () => {
+    signature?.clear();
+  };
+
+  const handleUndo = () => {
+    const lines = signature?.toData() || [];
+    lines.pop();
+    signature?.fromData(lines);
+  };
+  useEffect(() => {
+    if (signature && pageData?.length > 0) {
+     signature.fromData(pageData[pageIndex])
+    }
+  }, [pageData, pageIndex, signature]);
+
   return (
-    <div className="w-full h-[100vh] my-auto">
+    <div>
+      <SignatureCanvas
+        ref={setSignature}
+        canvasProps={{ width: 500, height: 200 }}
+      />
+      <button onClick={handleClear}>Clear</button>
+      <button onClick={handleUndo}>Undo</button>
+      <button onClick={() => {
+        if (signature) {
+          const data = signature.toData()
+          const updatedPageData = [...pageData]; // copia la matriz actual
+          updatedPageData[pageIndex] = data; // actualiza el elemento en el índice i
+          setPageData(updatedPageData); // actualiza el estado del componente con la nueva matriz
+          setPageIndex(pageIndex > 0 ? pageIndex - 1 : 0)
+        }
 
-      <div className=" w-full flex justify-center items-center">
-        <CanvasDraw className="" {...CanvasDrawProp} ref={canvasRef} />
-      </div>
-
-      <div className="flex  gap-6 justify-center items-center">
-
-
-
-<button onClick={() => {
-setCanvasDrawProps({ ...CanvasDrawProp, brushColor: "#FFFFFFFF" });
-}}>negro</button>
-<button >borrar</button>
-<button>guardar</button>
-<button >cargar</button>
-<div onClick={() => {
-      if (canvasRef) {
-        canvasRef.current?.undo();
-      }
-}}>undo</div>
-</div>
+      }}>before page</button>
+      <button onClick={() => {
+        if (signature) {
+          const data = signature.toData()
+          const updatedPageData = [...pageData]; // copia la matriz actual
+          updatedPageData[pageIndex] = data; // actualiza el elemento en el índice i
+          if(updatedPageData[pageIndex +1] === undefined){
+            updatedPageData[pageIndex +1] = []
+          }
+          setPageData(updatedPageData); // actualiza el estado del componente con la nueva matriz
+          setPageIndex(pageIndex + 1)
+          handleClear()
+        }
+      }}>next page</button>
     </div>
   );
 }
