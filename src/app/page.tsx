6e65a9322b718e703aa6359dcd869211dd0495f5
flo-314 <julianflo1916@gmail.com>
2023-03-30 20:51:38 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 "use client";
 import SignatureCanvas from 'react-signature-canvas'
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function Home() {
   const [signature, setSignature] = useState<SignatureCanvas | null>(null);
   const [pageIndex, setPageIndex] = useState<number>(0)
   const [pageData, setPageData] = useState<Array<Array<SignaturePad.Point>>[]>([])
   const [trimImages, setTrimImages] = useState<string[]>([])
+  
+  let animationInterval = useRef<any>(null)
   const handleClear = () => {
     signature?.clear();
   };
 
+  const animatePages = () => {
+    setPageIndex(pageIndex => (pageIndex + 1) % pageData.length);
+  };
+  
   
   const handleUndo = () => {
     const lines = signature?.toData() || [];
@@ -22,13 +30,30 @@ export default function Home() {
      signature.fromData(pageData[pageIndex])
     }
   }, [pageData, pageIndex, signature]);
+ 
+ 
+
+
 
   return (
     <div >
+      <div>
+      {trimImages.length  > 0 && animationInterval.current === null ?       <img
+      src={trimImages[pageIndex -1]}
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        opacity: 0.3,
+        zIndex: -1,
+      }}
+    />: null}
+
+  
       <SignatureCanvas
         ref={setSignature}
-        canvasProps={{ width: 500, height: 200 }}
-      />
+        canvasProps={{ width: 700, height: 700 }}
+      /></div>
       <button onClick={handleClear}>Clear</button>
       <button onClick={handleUndo}>Undo</button>
       <button onClick={() => {
@@ -67,7 +92,8 @@ export default function Home() {
       
       <div className='flex gap-4'>
         {trimImages.map((page,index) => {
-         return <div key={index} >
+         return <div onClick={() => {          setPageIndex(index)
+         }} key={index} >
             <img 
             className="w-32  h-32 border-4 border-black"
           src={page} />
@@ -76,6 +102,24 @@ export default function Home() {
          
         })}
       </div>
+      <button onClick={() => {
+   animationInterval.current = setInterval(() => {
+    animatePages();
+
+   
+  }, 200); // cambie este valor para ajustar la velocidad de la animación
+}}>
+  Play
+</button>
+
+<button onClick={() => {
+  clearInterval(animationInterval.current);
+  animationInterval.current = null
+}}>
+  Stop
+</button>
+
+
 
     </div>
   );
